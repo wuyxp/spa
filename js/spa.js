3e287ac90552ad8b09704728ba985302bd6ab02f
wuyang <wuyang@cloudin.cn>
2017-03-11 17:28:15 +0800

    增加model函数

--- js/spa.js
+++ js/spa.js
@@ -1,5 +1,6 @@
 var spa = (function(){
     var initModule = function( $container ){
+        spa.model.initModule();
         spa.shell.initModule( $container );
     }
     return {initModule : initModule}
